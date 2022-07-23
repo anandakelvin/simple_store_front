2073f6155828c5804c37de48e40027932f841531
anandakelvin <ananda.kelvin@gmail.com>
2022-07-24 02:48:26 +0700

    add currencies slice

---
+++ src/features/currencies/currenciesSlice.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import client from "../../api/client";
+import CURRENCIES from "../../queries/currencies";
+
+const initialState = {
+	value: [],
+	status: "idle",
+	error: null,
+	selectedCurrency: null,
+};
+
+export const currenciesSlice = createSlice({
+	initialState,
+	name: "currencies",
+	reducers: {
+		setCurrencies(state, action) {
+			state.value = action.payload;
+		},
+	},
+	extraReducers(builder) {
+		builder
+			.addCase(fetchCurrencies.pending, function (state, action) {
+				state.status = "loading";
+			})
+			.addCase(fetchCurrencies.fulfilled, function (state, action) {
+				state.status = "succeeded";
+				state.error = null;
+				state.value = action.payload.data.currencies;
+			})
+			.addCase(fetchCurrencies.rejected, function (state, action) {
+				state.status = "failed";
+				state.error = action.payload.error;
+			});
+	},
+});
+
+export const fetchCurrencies = createAsyncThunk(
+	"currencies/fetchCurrencies",
+	async function () {
+		const response = await client.query({ query: CURRENCIES });
+		return response;
+	}
+);
+
+export const { setCurrencies } = currenciesSlice.actions;
+export default currenciesSlice.reducer;
