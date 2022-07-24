4c0104fc1a2aea859022683e46805273383d3f62
anandakelvin <ananda.kelvin@gmail.com>
2022-07-25 01:51:43 +0700

    make string can be capitalized at first char in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,12 @@
 import "../src/index.css";
 
+// eslint-disable-next-line no-extend-native
+Object.defineProperty(String.prototype, "capitalize", {
+	value: function () {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	},
+});
+
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
 	controls: {
