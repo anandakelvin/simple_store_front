1e26146d700b066f794ac0efaab83680f5298a0d
anandakelvin <ananda.kelvin@gmail.com>
2022-07-28 16:54:51 +0700

    add storybook decorator to wrap any stories with redux store and memoryrouter

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
 import "../src/index.css";
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { MemoryRouter } from "react-router";
+import { Provider } from "react-redux";
+import store from "../src/app/store";
+
+addDecorator((story) => <Provider store={store}>{story()}</Provider>);
+addDecorator((story) => (
+	<MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+));
 
 // eslint-disable-next-line no-extend-native
 Object.defineProperty(String.prototype, "capitalize", {
